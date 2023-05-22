d725a2b65f09ef0ecd0e5259c570fe32c3f0f001
Yuri Ramos <yuri@smartlocator.net>
2023-05-22 19:46:32 -0300

    fix : unnecessary information

--- messenger-business-api/types.py
+++ messenger-business-api/types.py
@@ -4,12 +4,6 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class MessageTypes(Enum):
-    text = 'text'
-    image = 'image'
-
-
-
 class ApiRequestPayload(BaseModel):
     recipient: dict
     messaging_type: str = "Response"
