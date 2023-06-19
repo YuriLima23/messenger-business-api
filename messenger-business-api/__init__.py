3078a4e7e6d3a9b08d199a0c99a933bdc80a381b
Yuri Ramos <yuri@smartlocator.net>
2023-06-19 19:04:55 -0300

    first version api messenger lib in python send messages and download files like images, audio and video

--- messenger-business-api/__init__.py
+++ messenger-business-api/__init__.py
@@ -2,4 +2,4 @@
 
 __version__ = '0.1.0'
 
-from ..api import WhatsappAPI
+from .api import MessengerAPI
--- messenger-business-api/api.py
+++ messenger-business-api/api.py
@@ -1,24 +1,30 @@
-
-
-from typing import Dict, Union
-
-import requests
-
-from .exceptions import SendingMessageFailedException
-from .types import ApiRequestPayload, OutgoingTextPayload
+from typing import Dict, Optional
+from requests import Response
+from .execeptions import SendingMessageFailedException
+from .types import ApiRequestPayload
 
 
 class MessengerAPI:
 
-    def __init__(self,  access_token, api_version: str = 'v16.0'):
+    def __init__(self,  access_token: str, page_id: str, api_version: str = 'v16.0'):
         self.access_token = access_token
-        self.base_url = f'https://graph.facebook.com/{api_version}/'
+        self.base_url = f'https://graph.facebook.com/{api_version}/{page_id}'
+        self.headers: Dict[str, str] = {
+            'Content-Type': 'application/json',
+        }
 
-    def send_message_text(self, page_id: str, message):
-        payload = ApiRequestPayload(recipient={"id": page_id}, message=message)
-        result = requests.post(
-            self.base_url + f'/{page_id}/messages/access_token={self.access_token}', json=payload.json())
-    
+    def send_text_message(self, user_id: str, message):
+        payload = ApiRequestPayload(
+            recipient={"id": user_id}, message={"text": message})
+        url = f"{self.base_url}/messages?access_token={self.access_token}"
+        result = requests.post(url, headers=self.headers, data=payload.json())
+
         if result.status_code != 200:
             raise SendingMessageFailedException(result.status_code)
         return result
+
+    def get_media(self, uri: str) -> Optional[Response]:
+        # tratar os outros tipos de midia
+        response = requests.get(uri, stream=True)
+        response.raise_for_status()
+        return response
